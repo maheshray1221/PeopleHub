2707d65e1ec64771cb274e63185f0a8d084050a8
maheshray1221 <mr5832388@gmail.com>
2026-02-08 11:58:44 +0530

    Post model complete

---
+++ backend/src/models/posts.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+
+const commentSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    text: {
+        type: String,
+        required: true,
+        minlength: 1
+    }
+}, { timestamps: true })
+
+const postSchema = new mongoose.Schema(
+    {
+        image: {
+            type: String,  // cloudinary url
+            public_id: { type: String },
+            url: { type: String },
+        },
+        description: {
+            type: String,
+            required: true,
+            minlength: [10, "Description must be minimum 6 character"],
+            minlength: [50, "Description must be with in 25 character"],
+        },
+        likes: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ],
+        comments: [commentSchema],
+
+    }, { timestamps: true }
+)
+
+const Post = mongoose.model("Post",postSchema)
+
+export default Post
