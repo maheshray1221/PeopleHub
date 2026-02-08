9a790b0dfeb1fc3b0ed871c0cfe123ad1f4f188f
maheshray1221 <mr5832388@gmail.com>
2026-02-08 12:23:25 +0530

    all methodes in user model are completed

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userSchema = new mongoose.Schema(
     {
@@ -32,6 +34,54 @@ const userSchema = new mongoose.Schema(
         }
 
     }, { timestamps: true })
+
+// convert password in hash form
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+// coustom method for check password
+userSchema.methods.isPasswordCorrect = async (password) => {
+    return await bcrypt.compare(password, this.password)
+}
+
+
+// Generate Access Token
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            //payload
+            _id: this.id,
+            username: this.username,
+            email: this.email
+        },
+        // secret
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            // expire
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRE,
+        }
+    )
+}
+
+// Generate Refresh Token
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            //payload
+            _id: this.id,
+
+        },
+        // secret
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            // expire
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRE,
+        }
+    )
+}
 
 const User = mongoose.model("User", userSchema)
 
